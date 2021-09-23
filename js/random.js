db6426df1f8a91554037e4362da43013d70db56c
cl_cassanelli <erbapippo155@gmail.com>
2021-09-23 17:53:00 +0200

    1.0

---
+++ js/random.js
@@ -0,0 +1,16 @@
+function Randomize() {
+    var images = [
+        'Sono sicuro che il tuo culetto starebbe bene nella nostra chat', 
+        'Che ne pensi di mostrarmi la tua pesca?', 
+        'Sono amante della frutta, mi mostri la tua pesca?', 
+        'Me la mandi una foto di quel culetto?', 
+        'Giuro che se mi mandi una foto del tuo culetto inizio a fare il bravo',
+        'Mi hanno detto che le foto del culetto ti rendono felice, io sono molto triste oggi',
+        'GIuro che inizio a studiare se mi mandi una fotina'
+    ];
+    
+    var imageNum = Math.floor(Math.random() * images.length);
+    document.querySelector('.random').innerHTML = images[imageNum];
+}
+
+window.onload = Randomize
